1dac8dc590ebcb55a07be50b6e17514eb378ed20
Pauling <paulingzhou@gmail.com>
2018-01-22 19:37:48 +0800

    final commit
    时间分布优化结束，最后一次提交

--- get_cost_for_optimize.m
+++ get_cost_for_optimize.m
@@ -4,22 +4,33 @@ function [ total_cost ] = get_cost_for_optimize( subsidy )
 % fprintf('new calculate instance\n');
 % load('go_off_simulate.mat');
 load('swap_info_6330.mat');
-% subsidy = [0.0118728359156564;0.0130365974221455;0.0125850351236082;0.0134879886399743;0.0267050253827609;0.0634923123281603;0.140361556206378;0.268088696182059;0.177855557818379;0.0291217034067656;0;0;0.0439489148123318;0.382525859411672;0.332251560837528];
-if size(subsidy,1)==1
-    subsidy = subsidy';
+subsidy = [0.03,0.06,0.13,0.13,0.03,0.00,0.00,0.04,0.29,0.17,0.07,0.00,0.00]; 
+
+% if size(subsidy,1)==1
+%     subsidy = subsidy';
+% end
+if~(subsidy(6) >= subsidy(7) && subsidy(7) <= subsidy(8))
+    total_cost = NaN;
+else
+    subsidy = abs(subsidy);
+    subsidy_tenmin = zeros(144,1);
+    subsidy_tenmin(85:97) = subsidy;
+    subsidy_minute = get_subsidy_minute(subsidy_tenmin,10);
+%     subsidy_minute = zeros(1440,1);
+
+    swap_distribution_possibility = get_demand_ratio_via_subsidy(subsidy_minute);
+
+    % [swap_info_day1, swap_info_day2] = get_battery_swap_distribution(go_off_simulate_Day1,go_off_simulate_Day2, swap_distribution_possibility);
+    % demand_time_dist = get_demand_time_dist(swap_info_day1, swap_info_day2,1);
+    % swap_time_dist = get_swap_dist(demand_time_dist,1);
+    [demand_time_dist_sum,swap_time_dist_update] = update_swap_time_dist(swap_period,swap_distribution_possibility);
+%     max_swap_equip = sum(swap_time_dist_update(1:10));
+%     current_swap_equip = zeros(24*60,1);
+%     current_swap_equip(10) = max_swap_equip;
+%     for i = 11:1440
+%         current_swap_equip(i) = current_swap_equip(i-1)-swap_time_dist_update(i-10)+swap_time_dist_update(i);
+%     end
+    total_cost = get_total_cost(demand_time_dist_sum,swap_time_dist_update,subsidy_minute)/6550;
 end
-subsidy = abs(subsidy);
-subsidy_tenmin = zeros(144,1);
-subsidy_tenmin(80:94) = subsidy;
-subsidy_minute = get_subsidy_minute(subsidy_tenmin,10);
-% subsidy_minute = zeros(1440,1);
-
-swap_distribution_possibility = get_demand_ratio_via_subsidy(subsidy_minute);
-
-% [swap_info_day1, swap_info_day2] = get_battery_swap_distribution(go_off_simulate_Day1,go_off_simulate_Day2, swap_distribution_possibility);
-% demand_time_dist = get_demand_time_dist(swap_info_day1, swap_info_day2,1);
-% swap_time_dist = get_swap_dist(demand_time_dist,1);
-[demand_time_dist_sum,swap_time_dist_update] = update_swap_time_dist(swap_period,swap_distribution_possibility);
-total_cost = get_total_cost(demand_time_dist_sum,swap_time_dist_update,subsidy_minute)/6550;
 end
 
--- optimizeSubsidy_CMAES.m
+++ optimizeSubsidy_CMAES.m
@@ -1,6 +1,7 @@
 clc;
 clear;
 % cmaes('get_cost_for_optimize', 1.8*zeros(8,1),1);
+
     opts.LBounds = 0; opts.UBounds = 1.5; 
 %     opts.Restarts = 3;  % doubles the popsize for each restart
-    X = cmaes('get_cost_for_optimize', 1.5*zeros(15,1),0.6, opts);
+    X = cmaes('get_cost_for_optimize', 1.5*zeros(13,1),0.6, opts);
